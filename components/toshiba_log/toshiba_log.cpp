1ccd28662870a271bcef78feb0adec7164a653f5
Bart Theeuwes (Ampel) <229742795+bart-theeuwes-ampel@users.noreply.github.com>
2025-12-14 19:09:09 +0100

    * add source files

---
+++ components/toshiba_log/toshiba_log.cpp
@@ -0,0 +1,20 @@
+#include "toshiba_log.h"
+#include "esphome/core/log.h"
+
+namespace toshiba_log {
+
+static const char *TAG = "toshiba_log";
+
+void ToshibaLog::setup() {
+  ESP_LOGI(TAG, "UART logger started");
+}
+
+void ToshibaLog::loop() {
+  while (available()) {
+    uint8_t b;
+    read_byte(&b);
+    ESP_LOGD(TAG, "RX: %02X", b);
+  }
+}
+
+}  // 
