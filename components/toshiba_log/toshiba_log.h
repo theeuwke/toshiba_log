8ba1cc79767bf880530aa7d08e44e899498842d1
Bart Theeuwes (Ampel) <229742795+bart-theeuwes-ampel@users.noreply.github.com>
2025-12-14 19:16:39 +0100

    first commit

---
+++ components/toshiba_log/toshiba_log.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "esphome/core/component.h"
+#include "esphome/components/uart/uart.h"
+
+namespace toshiba_log {
+
+class ToshibaLog : public esphome::Component,
+                     public esphome::uart::UARTDevice {
+  public:
+    void setup() override;
+    void loop() override;
+  };
+}  // namespace toshiba_log
